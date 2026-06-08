661c8ce2b3a43bae4948e50bb71ca995d3670911
Ower BT <owerbt@gmail.com>
2026-06-07 21:39:25 -0300

    aula1_2

---
+++ aula1.py
@@ -0,0 +1,24 @@
+# Comentarios en python
+print(123) # Se pueden escribir comentarios al lado del codigo
+# Abajo tambien
+print("Hola mundo")
+
+'''
+Docstring usado dentro del codigo y sera usado para entender una API 
+el interpretador del codigo , consigue ler esta información
+'''
+
+# funcion print
+print() # función usada para exibir valores en el terminal
+print(12345)
+
+# Definiendo un tipo para la variable (TYPE HINTS)
+name: str = "python"
+
+print(f'"TESTE{name}"')
+
+# separador
+print(12 ,13 ,14 , 15 , sep = "-")
+
+# Quebra de linea
+print(12 ,13, 14, 15, sep="\n")
---
+++ aula2.py
@@ -0,0 +1,49 @@
+"""
+Introducción a los tipos de datos
+
+str -> String -> Texto = Textos que estan dentro de comillas
+"""
+
+# siguiendo las aulas usando la funciona print
+print(1234)
+
+# Usando comillas simples para str
+print('python automatico')
+
+# Usando comillas dobles para str
+print("python ejemplo") 
+
+# Comillas simples dentro de comillas dobles
+print("'Hola \"API\"'")
+
+# Tipos int e float
+# int numero inteiro
+# float numero decimais
+
+# função type mostrara el tipo da función
+
+print(152)
+print(-152)
+print(type(-52)) # outpout de valor entero negativo
+
+# float
+print(152.58)
+print(25.2)
+print(type(100.69))
+
+# Tipo de dados booleanos
+# al cuestionar algo en un programa
+# solo existen dos opciones posibles True or False
+# operadora mas comun, dentro de ellos == igual a 
+
+'''
+== Igual que
+!= diferente de 
+>= mayor que 
+<= menor que
+'''
+valor1: int = 100 
+valor2: int = 105
+resultado = valor1 != valor2
+print(resultado)
+print(type(resultado))
